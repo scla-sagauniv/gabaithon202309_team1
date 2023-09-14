087b5166df839adf23aabd9a4406a7df81de6d36
TOTDEM <tottyplay2486@gmail.com>
2023-09-15 08:36:22 +0900

    resultPage簡単な実装

--- react-app/src/components/Result.js
+++ react-app/src/components/Result.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Result.css'
 
 function Result() {
   return (
     <div>
-      <h1>結果発表!!!</h1>
-      <div>
+      <p className="winner_text">WINNER</p>
+      <div className="cha_win_rectangle">
+        <img className="cha_win_img" src="./img/nigaoe_leibniz.png" prop="leibniz"></img>
+      </div>
+      <div className="result">
+        <div className="result_top"></div>
+        <p className="result_result">RESULT</p>
+        <p className="result_time">対戦時間    </p>
+        <p className="result_rally">ラリー回数  </p>
+      </div>
+      <div className="button_tomenu1">
         <Link to="/SelectMode">
-          <button>モード選択画面へ</button>
+          <button className="button_tomenuforresult">モード選択画面へ</button>
         </Link>
       </div>
     </div>
