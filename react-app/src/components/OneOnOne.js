f66f166092d1c6ced60f3f4d6ab7092f2632da05
dorapon48 <dorapon48@icloud.com>
2023-09-15 02:32:38 +0900

    fix: 不要な物を整理

--- react-app/src/components/OneOnOne.js
+++ react-app/src/components/OneOnOne.js
@@ -2,64 +2,30 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import io from "socket.io-client";
 
-class OneOnOne extends React.Component{
-  constructor(props){
-    super(props);
+function OneOnOne(){
 
-    this.state = {
-        username: '',
-        message: '',
-        messages: []
-    };
+  const socket = io('localhost:8080', { withCredentials: true });
 
-    this.socket = io('localhost:8080', { withCredentials: true });
+  socket.on('RECEIVE_MESSAGE', function(data){
+      
+  });
 
-    this.socket.on('RECEIVE_MESSAGE', function(data){
-        addMessage(data);
-    });
-    // ルームIDの受け取り
-    this.socket.on('RECEIVE_ROOMID', function(data){
-        localStorage.setItem('ROOM_ID', data.room_id);
-    });
-
-    const addMessage = data => {
-        console.log(data);
-        this.setState({messages: [...this.state.messages, data]});
-        console.log(this.state.messages);
-    };
-
-    this.sendWord = ev => {
-        ev.preventDefault();
-        this.socket.emit('SEND_WORD', {
-            author: this.state.username,
-            word: this.state.message,
-            room_id: localStorage.getItem('ROOM_ID')
-        });
-        this.setState({message: ''});
-    }
-
-    this.enterTheRoom = ev => {
-        ev.preventDefault();
-        this.socket.emit('INTO_ROOM');
-    }
-  }
-
-  render(){
-    return (
+  // 初期データの受け取り
+  socket.on('SEND_BATTLE_INFO', function(data){
+      localStorage.setItem('ROOM_ID', data.room_id);
+  });
+  
+  return (
+    <div>
+      <h1>バトル!!</h1>
       <div>
-        <h1>バトル!!</h1>
-        <div>
-
-        <input type="text" placeholder="Message" value={this.state.message} onChange={ev => this.setState({message: ev.target.value})} className="form-control"/>
-          <Link to="/Result">
-            <button>終わり</button>
-          </Link>
-          <br/>
-          <button onClick={this.sendWord} >Send</button>
-        </div>
+        <input type="text" placeholder="Message" className="form-control"/>
+        <br/>
+        <button>Send</button>
       </div>
-    );
-  }
+    </div>
+  );
 }
+  
 
 export default OneOnOne;
